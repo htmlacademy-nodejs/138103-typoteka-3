8fa42d815c31255721ec2c0b588d7311798eef0c
joi-joe-joy <joi.joe.joy@mail.ru>
2020-05-21 13:18:34 +0300

    module2-task3

--- src/constants.js
+++ src/constants.js
@@ -1,18 +1,31 @@
+'use strict';
+
 const DEFAULT_COUNT = 1;
 const DEFAULT_COMMAND = `--generate`;
 const USER_ARGV_INDEX = 2;
 const ExitCode = {
-    success: 0,
-    error: 1
+  success: 0,
+  error: 1
 };
 const FILE_NAME = `mocks.json`;
 const MAX_PUBLICATIONS = 1000;
+const DEFAULT_PORT = 3000;
+
+const HttpCode = {
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401,
+};
 
 module.exports = {
-    DEFAULT_COUNT,
-    FILE_NAME,
-    MAX_PUBLICATIONS,
-    DEFAULT_COMMAND,
-    USER_ARGV_INDEX,
-    ExitCode
+  DEFAULT_COUNT,
+  FILE_NAME,
+  MAX_PUBLICATIONS,
+  DEFAULT_COMMAND,
+  USER_ARGV_INDEX,
+  DEFAULT_PORT,
+  ExitCode,
+  HttpCode
 };
--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,17 +1,16 @@
 'use strict';
 
 const {
-    getRandomInt,
-    shuffle,
+  getRandomInt
 } = require(`../../utils`);
 
 const {
-    DEFAULT_COUNT,
-    FILE_NAME,
-    MAX_PUBLICATIONS,
+  DEFAULT_COUNT,
+  FILE_NAME,
+  MAX_PUBLICATIONS,
 } = require(`../../constants`);
 
-let moment = require('moment');
+let moment = require(`moment`);
 const fs = require(`fs`).promises;
 const chalk = require(`chalk`);
 
@@ -20,65 +19,66 @@ const FILE_TITLES_PATH = `./data/titles.txt`;
 const FILE_CATEGORIES_PATH = `./data/categories.txt`;
 
 function createRandomDate() {
-    // startDate и endDate в формате timestamp
-    const startDate = moment().subtract(3, 'months').format('x');
-    const endDate = moment().format('x');
-    const randomDate = getRandomInt(startDate, endDate);
-    return moment(randomDate).format('YYYY-MM-DD HH:mm:ss')
+  // startDate и endDate в формате timestamp
+  const startDate = moment().subtract(3, `months`).format(`x`);
+  const endDate = moment().format(`x`);
+  const randomDate = getRandomInt(startDate, endDate);
+  return moment(randomDate).format(`YYYY-MM-DD HH:mm:ss`);
 }
 
 const createAnnounce = (sentences) => {
-    return Array(getRandomInt(1, 5)).fill('').map(() => sentences[getRandomInt(0, sentences.length - 1)]).join(` `);
-}
+  return Array(getRandomInt(1, 5)).fill(``).map(() => sentences[getRandomInt(0, sentences.length - 1)]).join(` `);
+};
 
 const createFullText = (sentences) => {
-    return Array(getRandomInt(0, sentences.length - 1)).fill('').map(() => sentences[getRandomInt(0, sentences.length - 1)]).join(` `);
-}
+  return Array(getRandomInt(0, sentences.length - 1)).fill(``).map(() => sentences[getRandomInt(0, sentences.length - 1)]).join(` `);
+};
+
 const createCategory = (categories) => {
-    return Array(getRandomInt(0, categories.length - 1)).fill('').map(() => categories[getRandomInt(0, categories.length - 1)]);
-}
+  return Array(getRandomInt(0, categories.length - 1)).fill(``).map(() => categories[getRandomInt(0, categories.length - 1)]);
+};
 
 const readContent = async (filePath) => {
-    try {
-        const content = await fs.readFile(filePath, `utf8`);
-        return content.split(`\n`);
-    } catch (err) {
-        console.error(chalk.red(err));
-        return [];
-    }
-}
+  try {
+    const content = await fs.readFile(filePath, `utf8`);
+    return content.split(`\n`);
+  } catch (err) {
+    console.error(chalk.red(err));
+    return [];
+  }
+};
 
 const generateArticles = (count, titles, sentences, categories) => (
-    Array(count).fill({}).map(() => ({
-        title: [titles[getRandomInt(0, titles.length - 1)]],
-        createdDate: createRandomDate(),
-        announce: createAnnounce(sentences),
-        fullText: createFullText(sentences),
-        сategory: createCategory(categories),
-    }))
+  Array(count).fill({}).map(() => ({
+    title: [titles[getRandomInt(0, titles.length - 1)]],
+    createdDate: createRandomDate(),
+    announce: createAnnounce(sentences),
+    fullText: createFullText(sentences),
+    сategory: createCategory(categories),
+  }))
 );
 
 module.exports = {
-    name: `--generate`,
-    async run(args) {
-        const titles = await readContent(FILE_TITLES_PATH);
-        const sentences = await readContent(FILE_SENTENCES_PATH);
-        const categories = await readContent(FILE_CATEGORIES_PATH);
+  name: `--generate`,
+  async run(args) {
+    const titles = await readContent(FILE_TITLES_PATH);
+    const sentences = await readContent(FILE_SENTENCES_PATH);
+    const categories = await readContent(FILE_CATEGORIES_PATH);
 
-        const [count] = args;
-        const countArticles = Number.parseInt(count, 10) || DEFAULT_COUNT;
-        if (countArticles <= MAX_PUBLICATIONS) {
-            const content = JSON.stringify(generateArticles(countArticles, titles, sentences, categories));
-            try {
-                await fs.writeFile(FILE_NAME, content);
-                console.info(chalk.green(`Operation success. File created.`));
-                process.exit(1);
-            } catch (err) {
-                console.info(chalk.red(`Can't write data to file...`));
-                process.exit(1);
-            }
-        } else {
-            console.info(chalk.red('Вы можете сгенерировать не более 1000 публикаций'));
-        }
+    const [count] = args;
+    const countArticles = Number.parseInt(count, 10) || DEFAULT_COUNT;
+    if (countArticles <= MAX_PUBLICATIONS) {
+      const content = JSON.stringify(generateArticles(countArticles, titles, sentences, categories));
+      try {
+        await fs.writeFile(FILE_NAME, content);
+        console.info(chalk.green(`Operation success. File created.`));
+        process.exit(1);
+      } catch (err) {
+        console.info(chalk.red(`Can't write data to file...`));
+        process.exit(1);
+      }
+    } else {
+      console.info(chalk.red(`Вы можете сгенерировать не более 1000 публикаций`));
     }
-}
+  }
+};
